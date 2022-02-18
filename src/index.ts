4c8c046a9bb09924d5132e3fd12df52cb2dfb4a8
Jeff <rustyangel@gmail.com>
2022-02-18 13:49:36 -0700

    New: Add `checkTitle` output options
    Allows for customising response level. Also returns an object of results
    to allow consumers to craft their own response message.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import nlp from 'compromise'
 // Provides dev-time type structures for  `danger` - doesn't affect runtime.
 import { DangerDSLType } from 'danger/distribution/dsl/DangerDSL.js'
-import nlp from 'compromise'
+
+import {
+	IMPERATIVE_ERROR,
+	IMPERATIVE_TIP,
+	LENGTH_ERROR,
+	LENGTH_TIP,
+	LENGTH_WARNING,
+} from './messages.js'
 
 declare const danger: DangerDSLType
 // Declare function message(message: string): void
@@ -8,44 +16,91 @@ declare function warn(message: string): void
 declare function fail(message: string): void
 declare function markdown(message: string): void
 
+interface CheckTitleOptions {
+	/**
+	 * Error type for imperative mood check.
+	 * @default 'fail'
+	 */
+	imperativeMood?: false | 'warn' | 'fail'
+	/**
+	 * Should a character limit be enforced.
+	 * @default true
+	 */
+	titleLength?: boolean
+	returnOnly?: boolean
+}
+
+type CheckResult = 'pass' | 'warn' | 'fail'
+
+interface CheckOutput {
+	result?: CheckResult
+	message?: string
+	tip?: string
+}
+
 /**
  * Validate PR titles
  *
  * Checks for subject length, capitalisation, trailing punctuation,
  * and imperative mood.
  */
-export function checkTitle() {
-	let { title } = danger.github.pr
-	/** @todo Ignore "tag" prefixes. */
-	let isImperative = Boolean(nlp(title).verbs().isImperative().text())
-
-	if (!isImperative) {
-		fail('PR titles should use the imperative mood.')
-		markdown(`
-*Imperative mood* just means “spoken or written as if giving a command
-or instruction”.
-
-💡 **Tip:** A properly formed Git commit subject line should always be
-able to complete the following sentence:
-
-> If applied, this commit will…
-		`)
+export function checkTitle(title?: string, options?: CheckTitleOptions) {
+	title ||= danger.github.pr.title
+	let { imperativeMood, titleLength, returnOnly } = options || {
+		imperativeMood: 'fail',
+		titleLength: true,
+		returnOnly: false,
 	}
 
-	if (50 <= title.length && title.length <= 72) {
-		warn('Great PR titles are 50 characters or fewer.')
+	let imperativeMoodOutput: CheckOutput
+	if (imperativeMood) {
+		/** @todo Ignore "tag" prefixes. */
+		let isImperative = Boolean(nlp(title).verbs().isImperative().text())
+
+		if (!isImperative && !returnOnly) {
+			if (imperativeMood === 'fail') fail(IMPERATIVE_ERROR)
+			if (imperativeMood === 'warn') warn(IMPERATIVE_ERROR)
+			markdown(IMPERATIVE_TIP)
+		}
+
+		imperativeMoodOutput = {
+			result: isImperative ? 'pass' : imperativeMood,
+			message: !isImperative && IMPERATIVE_ERROR,
+			tip: !isImperative && IMPERATIVE_TIP,
+		}
 	}
 
-	if (72 < title.length) {
-		fail(
-			'PR titles must be less than 72 characters to avoid being truncated.'
-		)
+	let titleLengthOutput: CheckOutput
+	if (titleLength) {
+		let lengthResult: CheckResult = 'pass'
+		let lengthMessage: string
+		if (50 <= title.length && title.length <= 72) {
+			if (!returnOnly) warn(LENGTH_WARNING)
+			lengthResult = 'warn'
+			lengthMessage = LENGTH_WARNING
+		}
+
+		if (72 < title.length) {
+			if (!returnOnly) fail(LENGTH_ERROR)
+			lengthResult = 'fail'
+			lengthMessage = LENGTH_ERROR
+		}
+
+		let showLengthTip = false
+		if (50 <= title.length) {
+			if (!returnOnly) markdown(LENGTH_TIP)
+			showLengthTip = true
+		}
+
+		titleLengthOutput = {
+			result: lengthResult,
+			message: lengthMessage,
+			tip: showLengthTip && LENGTH_TIP,
+		}
 	}
 
-	if (50 <= title.length) {
-		markdown(`
-💡 **Tip:** If you’re having a hard time summarizing, you might be
-committing too many changes at once.
-		`)
+	return {
+		imperativeMood: imperativeMoodOutput,
+		titleLength: titleLengthOutput,
 	}
 }
